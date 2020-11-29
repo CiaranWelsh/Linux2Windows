ce97cf3d1bd52bdbcae152eae65be3d3b4134988
CiaranWelsh <cwelsh2@uw.edu>
2020-11-29 02:05:04 +0000

    usleep is a function inside unistd, so fixed the bug introduced by treating it as a header

--- include/linux2win/linux2win_usleep.h
+++ include/linux2win/linux2win_usleep.h
@@ -3,14 +3,8 @@
 
 #include "linux2win_config.h"
 
-
-#ifdef HAVE_USLEEP_H
-#   include <usleep.h>
-#else
-
+#ifndef HAVE_USLEEP_H
 #   include "linux2win/_usleep.h"
-
 #endif
-
 
 #endif //LINUX2WIN_USLEEP_H
